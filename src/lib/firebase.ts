f58c8186410d6b49f710fa580f1b26f6c33a8eaf
Yash Saradva <yashsaradva0@gmail.com>
2025-08-26 12:00:32 +0000

    Why am I getting Firebase: Error (auth/configuration-not-found) when sig

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "anistream-z88tx",
-  appId: "1:645190001140:web:e8cb548fc8ab2b122319a1",
-  storageBucket: "anistream-z88tx.firebasestorage.app",
   apiKey: "AIzaSyAwTQg4ZoJb5aDLCAoZRLQeu1yKt160YxA",
   authDomain: "anistream-z88tx.firebaseapp.com",
+  projectId: "anistream-z88tx",
+  storageBucket: "anistream-z88tx.appspot.com",
   messagingSenderId: "645190001140",
-  measurementId: "G-65YPS7T5G5"
+  appId: "1:645190001140:web:e8cb548fc8ab2b122319a1"
 };
 
 // Initialize Firebase
